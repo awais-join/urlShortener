26c5f79336d1db19d4d32a0c4f54a6c6df2f0c40
Tariq,Awais <awais.tariqq@gmail.com>
2019-04-18 18:31:50 -0400

    added code for request and response handling in between view and controller.

--- Url-Shortener/src/main/java/com/application/urlShortener/Controller/UrlShortenerController.java
+++ Url-Shortener/src/main/java/com/application/urlShortener/Controller/UrlShortenerController.java
@@ -1,15 +1,35 @@
 package com.application.urlShortener.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.application.urlShortener.Model.URLObject;
 
 
 @Controller
 public class UrlShortenerController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String homePage() {
+    public String homePage(ModelMap map) {
+    	map.addAttribute("urlObject", new URLObject());
         return "index";
+    }
+    
+    @PostMapping("/")
+    public String greetingSubmit(@ModelAttribute URLObject urlObject, ModelMap map) {
+    	urlObject.setConvertedURL("123456");
+    	map.addAttribute("urlObject", urlObject);
+        return "index";
+    }
+    
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public String homePage(@PathVariable("id") int id) {
+        String redirectUrl = "http://www.google.com";
+    	return "redirect:" + redirectUrl;
     }
 }
